3d7f92ded6bdcf4d68862f97c235c0795e970aad
deepakgoautoticket <113604568+deepakgoautoticket@users.noreply.github.com>
2022-12-20 16:07:28 +0530

    Add files via upload

---
+++ JKPatternLock-master/db_info.php
@@ -0,0 +1,16 @@
+<?php
+
+$mysqli = new mysqli("localhost", "jayeshka", "1020Dennison", "jayeshka_network_2705085");
+
+
+//echo $mysqli;
+//Check connection 
+if (mysqli_connect_errno()) {
+    printf("Connect failed with Error: %s Exiting now...\n", mysqli_connect_error());
+    exit();
+}
+
+
+?>
+  
+ 
